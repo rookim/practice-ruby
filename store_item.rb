f2718c9060570a776fd5a5568b785d8da1c27916
ro <kronew6@gmail.com>
2022-01-05 12:31:40 -0600

    adds items to store list.

---
+++ store_item.rb
@@ -0,0 +1,19 @@
+
+brookside_dark_chocolate = {
+  color: "brown",
+  price: 4.99,
+}
+
+polka_dotted_headband = {
+  "color" => "pink and white",
+  "price" => 9.99,
+}
+
+iphone_thirteen_pro = {
+  :color => "sierra blue",
+  :price => 899,
+}
+
+p brookside_dark_chocolate
+p polka_dotted_headband
+p iphone_thirteen_pro
