a42b0c02c4b7807c0826158648787f18a3c66bc4
ro <kronew6@gmail.com>
2022-01-05 13:58:57 -0600

    creates item-represented class.

--- store_item.rb
+++ store_item.rb
@@ -1,19 +1,71 @@
 
-brookside_dark_chocolate = {
-  color: "brown",
-  price: 4.99,
-}
+# brookside_dark_chocolate = {
+#   color: "brown",
+#   price: 4.99,
+# }
 
-polka_dotted_headband = {
-  "color" => "pink and white",
-  "price" => 9.99,
-}
+# polka_dotted_headband = {
+#   "color" => "pink and white",
+#   "price" => 9.99,
+# }
 
-iphone_thirteen_pro = {
-  :color => "sierra blue",
-  :price => 899,
-}
+# iphone_thirteen_pro = {
+#   :color => "sierra blue",
+#   :price => 899,
+# }
 
-p brookside_dark_chocolate
-p polka_dotted_headband
-p iphone_thirteen_pro
+# p brookside_dark_chocolate
+# p polka_dotted_headband
+# p iphone_thirteen_pro
+
+#rewrite as a class
+class Store
+  def initialize(input_item_name, input_color, input_price)
+    @name = input_item_name
+    @color = input_color
+    @price = input_price
+  end
+
+  def name
+    @name
+  end
+
+  def color
+    @color
+  end
+
+  def price
+    @price
+  end
+
+  def price=(value)
+    @price = value
+  end
+
+  def print_price_info
+    puts "The #{name} is $#{price}."
+  end
+end
+
+item1 = Store.new("Brookside Dark Chocolate", "brown", 4.99)
+item2 = Store.new("Polka-dotted Headband", "pink and white", 9.99)
+item3 = Store.new("iPhone 13 Pro", "Sierra Blue", 899)
+
+puts item1.name
+puts item1.color
+puts item1.price
+item1.price = 2.99
+puts item1.price
+puts item1.print_price_info
+puts ""
+puts item2.name
+puts item2.color
+puts item2.price
+puts item2.print_price_info
+puts ""
+puts item3.name
+puts item3.color
+puts item3.price
+puts item3.print_price_info
+# p item2
+# p item3
