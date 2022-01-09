47f196c218b2f95b432de3c851ccd997b09d169a
ro <kronew6@gmail.com>
2022-01-09 00:35:25 -0600

    creates ruby_review.rb file

--- food.rb
+++ food.rb
@@ -1,10 +1,15 @@
 require "./store_item.rb"
 
 class Food 
-  attr_accessor :shelf_life
   include Store
+  attr_accessor :shelf_life
 
   def initialize (options_hash)
+    super
     @shelf_life = options_hash[:shelf_life]
   end
 end
+
+item1 = Food.new(name: "Brookside Dark Chocolate", color: "brown", price: 4.99, shelf_life: "6 months")
+# item2 = Store.new(name: "Polka-dotted Headband", color: "pink and white", price: 9.99, return_policy: "30 days")
+# item3 = Store.new(name: "iPhone 13 Pro", color: "Sierra Blue", price: 899, return_policy: "14 days")
--- not_food.rb
+++ not_food.rb
@@ -1,10 +1,11 @@
 require "./store_item.rb"
 
 class NotFood
-  attr_accessor :return_policy
   include Store
+  attr_accessor :return_policy
 
   def initialize (options_hash)
+    super
     @return_policy = options_hash[:return_policy]
   end
 end
